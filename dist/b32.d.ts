007f24869740ab0e5a36aa0298704afc2cc74b5d
Zane Hannan <zanehannanau@gmail.com>
2018-06-20 16:18:37 +1000

    Definitions: Overwrite never on string.

--- dist/b32.d.ts
+++ dist/b32.d.ts
@@ -2,7 +2,9 @@ declare const b32c: {
     [key: number]: number;
 };
 declare const xb: number[];
-declare function b32_buf(b32: ArrayBufferView | string, overwrite?: boolean): Uint8Array;
+declare function b32_buf(b32: ArrayBufferView, overwrite?: true | undefined): Uint8Array;
+declare function b32_buf(b32: string, overwrite?: boolean): Uint8Array;
+declare function b32_buf(b32: string, overwrite?: true): never;
 declare const b256: Uint8Array;
 declare function buf_b32(bv: ArrayBufferView, useString?: boolean): Uint8Array;
 declare function buf_b32(bv: ArrayBufferView, useString?: true): string;
