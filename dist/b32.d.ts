0e69936be026e8680c484f220f8be10bf9e95715
Zane Hannan <zanehannanau@gmail.com>
2018-06-20 14:44:14 +1000

    Decisivise the output of different inputs.

--- dist/b32.d.ts
+++ dist/b32.d.ts
@@ -4,6 +4,7 @@ declare const b32c: {
 declare const xb: number[];
 declare function b32_buf(b32: ArrayBufferView | string, overwrite?: boolean): Uint8Array;
 declare const b256: Uint8Array;
-declare function buf_b32(bv: ArrayBufferView, useString?: boolean): Uint8Array | string;
+declare function buf_b32(bv: ArrayBufferView, useString?: boolean): Uint8Array;
+declare function buf_b32(bv: ArrayBufferView, useString?: true): string;
 export { b32_buf, buf_b32, b32_buf as decode, buf_b32 as encode, b32c, b256, xb, };
 //# sourceMappingURL=b32.d.ts.map
